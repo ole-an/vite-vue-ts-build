342ab7f3595901e16e1f3c3a736da8e1b4dbeea8
Andrianov Oleg <ole-an@hotmail.com>
2025-02-06 22:38:18 +0300

    .prettierrc.cjs clarify settings

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -2,4 +2,9 @@ module.exports = {
   singleQuote: true,
   endOfLine: 'auto',
   tabWidth: 2,
+  proseWrap: 'always',
+  htmlWhitespaceSensitivity: 'ignore',
+  useTabs: false,
+  printWidth: 80,
+  singleAttributePerLine: true,
 };
