7fa29a4b1a1fdf4b70f1ae4f2c3588aba8f04aec
Andrianov Oleg <ole-an@hotmail.com>
2025-02-07 22:01:54 +0300

    main.ts base settings

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createPinia } from 'pinia';
+import { createApp } from 'vue';
+import { useI18n } from '@core/composables/i18n.ts';
+import { deviceType } from '@core/utils/getInputType.ts';
+import { setClassName } from '@core/utils/general.ts';
+import { router } from '@/router/router.ts';
+
+import App from '@/App.vue';
+import { registerLayouts } from '@/layouts/register.ts';
+
+const pinia = createPinia();
+
+export const app = createApp(App);
+
+app.use(pinia);
+app.use(router);
+registerLayouts(app);
+app.mount('#app');
+
+export const { $t } = useI18n();
+setClassName('html', 'input-', deviceType, 'startsWith');
