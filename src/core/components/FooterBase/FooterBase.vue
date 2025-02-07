1c2f0f7ea344d0a0214e9e77b4e035dfa651939b
Andrianov Oleg <ole-an@hotmail.com>
2025-02-07 21:35:50 +0300

    FooterBase component

---
+++ src/core/components/FooterBase/FooterBase.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <footer class="footer-base">Footer</footer>
+</template>
+
+<style lang="scss">
+@use 'footer-base';
+</style>
---
+++ src/core/components/FooterBase/index.ts
@@ -0,0 +1,1 @@
+export { default as FooterBase } from './FooterBase.vue';
