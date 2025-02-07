f3a152d107673799c17c8cb08e67f34992ced1d8
Andrianov Oleg <ole-an@hotmail.com>
2025-02-07 21:46:46 +0300

    src/core/layouts DefaultLayout

---
+++ src/core/layouts/DefaultLayout.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { HeaderBase } from '@core/components/HeaderBase';
+import { MainBase } from '@core/components/MainBase';
+import { FooterBase } from '@core/components/FooterBase';
+</script>
+
+<template>
+  <div class="default-layout">
+    <HeaderBase />
+    <MainBase>
+      <slot />
+    </MainBase>
+    <FooterBase />
+  </div>
+</template>
+
+<style lang="scss">
+.default-layout {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  min-height: 100vh;
+}
+</style>
