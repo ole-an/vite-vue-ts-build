f389e9409e8dcca83c5fa3163500481716e9aeee
Andrianov Oleg <ole-an@hotmail.com>
2025-02-07 21:45:44 +0300

    MainBase component

---
+++ src/core/components/MainBase/MainBase.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main class="main-base">
+    <div class="main-base__container container">
+      <slot></slot>
+    </div>
+  </main>
+</template>
+
+<style lang="scss">
+@use 'main-base';
+</style>
---
+++ src/core/components/MainBase/index.ts
@@ -0,0 +1,1 @@
+export { default as MainBase } from './MainBase.vue';
