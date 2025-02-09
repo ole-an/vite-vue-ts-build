c22db369019aab90066932edd6534ac4487f5c56
Andrianov Oleg <ole-an@hotmail.com>
2025-02-09 09:42:58 +0300

    TransitionFadeBase component

---
+++ src/core/components/TransitionFadeBase/TransitionFadeBase.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <Transition name="transition-fade-base">
+    <slot></slot>
+  </Transition>
+</template>
+
+<style lang="scss">
+@use 'transition-fade-base';
+</style>
---
+++ src/core/components/TransitionFadeBase/index.ts
@@ -0,0 +1,1 @@
+export { default as TransitionFadeBase } from './TransitionFadeBase.vue';
