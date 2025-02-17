3a1a43d3e8e489a961a5aa77cecfeb5ff1379bf0
Andrianov Oleg <ole-an@hotmail.com>
2025-02-17 19:58:37 +0300

    main.ts add i18n plugin reference, small edits

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,20 @@
-import { createPinia } from 'pinia';
+import { createPinia, Pinia } from 'pinia';
 import { createApp } from 'vue';
-import { i18n, useI18n } from '@core/composables/i18n.ts';
+import { i18n } from '@core/composables/i18n.ts';
 import { deviceType } from '@core/utils/getInputType.ts';
 import { setClassName } from '@core/utils/general.ts';
 import { router } from '@/router/router.ts';
-
 import App from '@/App.vue';
 import { registerLayouts } from '@/layouts/register.ts';
 
-const pinia = createPinia();
+const pinia: Pinia = createPinia();
 
 export const app = createApp(App);
 
 app.use(pinia);
-app.use(router);
 app.use(i18n);
+app.use(router);
 registerLayouts(app);
 app.mount('#app');
 
-export const { $t } = useI18n();
 setClassName('html', 'input-', deviceType, 'startsWith');
