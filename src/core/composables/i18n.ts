6119bb6f825d9d01b826832ef395e48d6a5358eb
Andrianov Oleg <ole-an@hotmail.com>
2025-02-17 19:53:32 +0300

    i18n.ts remove ITranslates, remove handmade useI18n function, setup locale input in i18n plugin object

--- src/core/composables/i18n.ts
+++ src/core/composables/i18n.ts
@@ -1,6 +1,5 @@
 import { createI18n } from 'vue-i18n';
 import { getFirstBrowserLanguage } from '@core/utils/getFirstBrowserLanguage.ts';
-import { useLocalesStore } from '@core/store/locales.store.ts';
 import { setClassName } from '@core/utils/general.ts';
 import {
   locales,
@@ -10,13 +9,7 @@ import {
 import en from '@locales/en/base.json';
 import ru from '@locales/ru/base.json';
 
-export interface ITranslates {
-  [key: string]: any /* необходимо добработать */;
-}
-
-const messages = { ru, en };
-
-function setLocale(locale?: string): string {
+export function setLocale(locale?: string): string {
   let appLocale: string = localStorage.getItem('locale') || '';
 
   if (!appLocale || locale) {
@@ -46,42 +39,8 @@ function setLocale(locale?: string): string {
   return appLocale;
 }
 
-export function useI18n() {
-  function initI18n(locale?: string): void {
-    setLocale(locale);
-  }
-
-  function $t(
-    key: string,
-    param: null | Array<string> = null,
-  ): ITranslates | string {
-    const store = useLocalesStore();
-    let val = key
-      .split('.')
-      .reduce(
-        (previousValue: ITranslates, currentValue: string) =>
-          previousValue[currentValue],
-        messages[store.currentLocale as keyof typeof messages],
-      );
-    if (param) {
-      val = val.replace('{0}', param);
-    }
-    return val;
-  }
-
-  return {
-    $t,
-    initI18n,
-    setLocale,
-  };
-}
-
-export const { $t, initI18n } = useI18n();
-
-initI18n();
-
 export const i18n = createI18n({
-  locale: 'ru',
+  locale: setLocale(),
   fallbackLocale: 'en',
   messages: {
     ru,
