8db034226e0962470b80d7bc19232c48db34badd
Andrianov Oleg <ole-an@hotmail.com>
2025-02-18 09:22:36 +0300

    HamburgerBase component

---
+++ src/core/components/HamburgerBase/HamburgerBase.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, unref } from 'vue';
+
+const isOpen = ref<boolean>(false);
+
+const toggleHamburger = () => {
+  isOpen.value = !unref(isOpen);
+};
+</script>
+
+<template>
+  <button
+    :class="{ 'is-active': isOpen }"
+    class="hamburger"
+    type="button"
+    @click="toggleHamburger"
+  >
+    <div class="hamburger__box">
+      <div class="hamburger__inner"></div>
+    </div>
+  </button>
+</template>
+
+<style lang="scss">
+@use 'hamburger-base';
+</style>
---
+++ src/core/components/HamburgerBase/index.ts
@@ -0,0 +1,1 @@
+export { default as HamburgerBase } from './HamburgerBase.vue';
